dbc636d7dad040658fab9e82cc69b49db335c938
karben86 <benjaminkarst86@gmail.com>
2020-11-13 15:48:57 +0000

    setting up db for heroku hosting

--- listen.js
+++ listen.js
@@ -1,4 +1,4 @@
 const app = require('./app.js');
-const { PORT = 9090 } = process.env;
+const { port = 9090 } = process.env;
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+app.listen(port, () => console.log(`Listening on ${port}...`));
